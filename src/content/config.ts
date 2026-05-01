066acae28059d6f199c5693bca49a4a14750794f
enoa153974 <aoi.153974@gmail.com>
2026-05-01 16:01:06 +0900

    ギャラリーページ構成変更前

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 // src/content/config.ts
 import { defineCollection, z } from 'astro:content';
 
+//コラムページのスキーマ設定
 const columns = defineCollection({
     type: 'content',
     schema: ({ image }) =>
@@ -12,4 +13,33 @@ const columns = defineCollection({
         }),
 });
 
-export const collections = { columns };
+
+//worksページの各制作物詳細ページのスキーマ設定
+const works = defineCollection({
+    type: 'content',
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            category: z.string(), // LP / Web / Banner
+            tags: z.array(z.string()).optional(),
+            thumbnail: image(),
+            publish: z.boolean().default(true),
+
+            // 👇ここが超重要
+            overview: z.string(),
+            target: z.string(),
+            problem: z.string(),
+            solution: z.string(),
+
+            // 👇スキル見せる
+            designPoints: z.string().optional(),
+            tech: z.array(z.string()).optional(),
+
+            // 👇あると強い
+            url: z.string().optional(),
+        }),
+});
+
+
+//コラムページ、制作物詳細ページのスキーマを定義
+export const collections = { columns , works};
